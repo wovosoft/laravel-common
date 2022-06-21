faf20150a97d84fdd43597a553c86f0d378bee30
Narayan Adhikary <wovosoft@gmail.com>
2022-06-21 19:09:06 +0600

    Added Messages and Data

---
+++ src/Helpers/Messages.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Wovosoft\LaravelCommon\Helpers;
+
+use Illuminate\Http\JsonResponse;
+
+class Messages
+{
+    public static function success(array $merge = []): JsonResponse
+    {
+        return response()->json(array_merge([
+            "message" => "Successfully Done"
+        ], $merge));
+    }
+
+    public static function failed(\Throwable $throwable, array $merge = []): JsonResponse
+    {
+        if (app()->environment("development")) {
+            return response()->json(array_merge([
+                "message" => $throwable->getMessage(),
+                "file" => $throwable->getFile(),
+                "line" => $throwable->getLine()
+            ], $merge), 403);
+        }
+        return response()->json(array_merge([
+            "message" => "Operation Failed",
+        ], $merge), 403);
+    }
+}
