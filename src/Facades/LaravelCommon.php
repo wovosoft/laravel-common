d16c5b1dcd399035ba4ba427e320bd33eff2ff12
Narayan Adhikary <wovosoft@gmail.com>
2022-06-21 18:43:45 +0600

    Initiated

---
+++ src/Facades/LaravelCommon.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Wovosoft\LaravelCommon\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class LaravelCommon extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return 'laravel-common';
+    }
+}
