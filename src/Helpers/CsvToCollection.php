51f029d23cc9aadab5318ebc052fc16f186e42d6
Narayan Adhikary <wovosoft@gmail.com>
2022-12-28 16:54:25 +0600

    Added CsvToCollection Class

---
+++ src/Helpers/CsvToCollection.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Wovosoft\LaravelCommon\Helpers;
+
+use Illuminate\Support\Collection;
+
+class CsvToCollection
+{
+    public static function convert(string $path): Collection
+    {
+        $file_to_read = fopen($path, 'r');
+        $index = 0;
+        $header = null;
+        $rows = collect([]);
+        while (!feof($file_to_read)) {
+            $line = fgetcsv($file_to_read, 1000, ',');;
+            if ($index === 0) {
+                $header = $line;
+            } else {
+                $row = [];
+                foreach ($header as $k => $h) {
+                    $key = str($h)->lower()->value();
+                    if (gettype($line) === "array") {
+                        $row[$key] = $line[$k];
+                    }
+                }
+
+                if (count($row)) {
+                    $rows->add($row);
+                }
+            }
+            $index++;
+        }
+
+        fclose($file_to_read);
+
+
+        return $rows;
+    }
+}
